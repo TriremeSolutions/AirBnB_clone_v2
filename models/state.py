b00fab4e8cb7bff0ab720679ec0e5bee8d25178f
TriremeSolutions <themellowread@gmail.com>
2023-06-24 23:34:10 +0100

    updating state.py: city-state filestorage relationship

--- models/state.py
+++ models/state.py
@@ -3,6 +3,37 @@
 from models.base_model import BaseModel
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """
+    State class, links to database table named "states"
+    Inherits from Base for the MySQL database
+    """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+
+    # define the rlationship with city table
+    cities = relationship("City",  backref="state" cascade="delete")
+
+    # define the FileStorage relationship with City
+    @property
+    def cities(self):
+        temp = []
+        # an interim placholder
+        city_instances = []
+        # list of all 'City' instances
+        for k in models.storage.all():
+            # check for City keys in the objects dict
+            entry = k.replace('.', ' ')
+            # parse object dict keys to separate id of key from class
+            entry = entry.split()
+            # break up string, placing object key in its own.
+            if (entry[0] == 'City'):
+                # check if key was a City instance
+                temp.append(models.storage.all()[k])
+                # add entire k-v entry to temp list
+            # now check temp list for items, if any
+            for item in temp:
+                if (item.state_id == self.id):
+                    # City's state_id value must match a "State" id
+                    city_instances.append(item)
+            return (city_instances)

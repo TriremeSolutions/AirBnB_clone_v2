2fde6a189ba2e34a9f892deb08b42191b333f9a6
TriremeSolutions <themellowread@gmail.com>
2023-07-14 23:27:55 +0100

    update user, review, place, city

--- models/user.py
+++ models/user.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
+# update user for SQL
+from models.base_model import Base
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
@@ -12,3 +17,4 @@ class User(BaseModel):
     last_name = Column(String(128))
     places = relationship("Place", backref="user", cascade="delete")
     reviews = relationship("Review", backref="user", cascade="delete")
+    

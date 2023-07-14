5056f6838bbc366493bf54a263fd1bb13bcf1ebe
TriremeSolutions <themellowread@gmail.com>
2023-07-15 00:23:53 +0100

    update do_all

--- console.py
+++ console.py
@@ -265,21 +265,20 @@ class HBNBCommand(cmd.Cmd):
 
     def do_all(self, args):
         """ Shows all objects, or all objects of a class"""
-        print_list = []
+        if not args:
+            sta = storage.all()
+            print([sta[k].__str__() for k in sta])
+            return
+        try:
+            args = args.split(" ")
+            if args[0] not in self.classes:
+                raise NameError()
 
-        if args:
-            args = args.split(' ')[0]  # remove possible trailing args
-            if args not in HBNBCommand.classes:
-                print("** class doesn't exist **")
-                return
-            for k, v in storage.all():
-                if k.split('.')[0] == args:
-                    print_list.append(str(v))
-        else:
-            for k, v in storage.all():
-                print_list.append(str(v))
+            sta = storage.all(eval(args[0]))
+            print([sta[k].__str__() for k in sta])
 
-        print(print_list)
+        except NameError:
+            print("** class doesn't exist **")
 
     def help_all(self):
         """ Help information for the all command """

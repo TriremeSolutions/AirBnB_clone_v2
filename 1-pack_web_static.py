3673436877e82c8d39d26d54475a088609ca4389
TriremeSolutions <themellowread@gmail.com>
2023-07-16 22:30:18 +0000

    wastrel

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,13 +9,16 @@ def do_pack():
     """
     Compresses web_static dir to a tar gzip archive.
     """
-    dt = datetime.now()
-    fmt = "%Y%m%d%H%M%S"
-    gzip = 'versions/web_static_{}.tgz'.format(dt.strftime(fmt))
-    if os.path.isdir("versions") is False:
-        if local("sudo mkdir -p versions").failed is True:
+    try:
+        dt = datetime.now()
+        fmt = "%Y%m%d%H%M%S"
+        path_gzip = "./versions/web_static_{}.tgz".format(dt.strftime(fmt))
+        if os.path.isdir("versions") is False:
+            if local("mkdir -p versions").failed is True:
+                return None
+        if local("tar -cvzf {} web_static".format(path_gzip)).failed is True:
             return None
-        if local("sudo tar -cvzf {} web_static".format(gzip)).failed is True:
-            return None
-        file_path = gzip
-        return file_path
+        return path_gzip
+
+    except Exception as e:
+        return None

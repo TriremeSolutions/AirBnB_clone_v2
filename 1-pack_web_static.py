daab1f8656f079ef174420d09d030474101729e1
TriremeSolutions <themellowread@gmail.com>
2023-07-16 20:27:23 +0100

    compress web_static with fabric script 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" FabScript that archives the contents of a folder """
+import os.path
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """
+    Compresses web_static dir to a tar gzip archive.
+    """
+    try:
+        if not os.path.exists("versions"):
+            local('mkdir versions')
+        dt = datetime.utcnow()
+        fmt = "%Y%m%d%H%M%S"
+        path_gzip = 'versions/web_static_{}.tgz'.format(dt.strftime(fmt))
+        local('tar -cvzf {} web_static'.format(path_gzip))
+        return path_gzip
+    except Exception as e:
+        return None

547460110bb08d64df3a4d85a5c223211fc0667f
TriremeSolutions <themellowread@gmail.com>
2023-07-17 18:45:50 +0100

    2 deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,38 +2,70 @@
 """ Function that deploys content of an archive """
 import os
 from datetime import datetime
-from fabric.api import env
-from fabric.api import put
-from fabric.api import run
+from fabric.api import *
+from fabric.operations import put
 import shlex
 
 env.hosts = ['100.26.244.129', '52.204.216.209']
 
 
+def do_pack():
+    """
+    move contents of web_static to .tgz archive
+    """
+    dt = datetime.utcnow().strftime("%Y%m%d%H%M%S")
+    local('mkdir -p versions')
+    test = local('tar -cvf versions/web_static_{}.tgz web_static'
+                 .format(dt))
+    if test.failed:
+        return None
+    else:
+        return test
+
+
 def do_deploy(archive_path):
-    """ Search and deploy archive to server """
-    if not os.path.exists(archive_path):
+    """ Search and deploy static archive to server """
+    if not os.path.isfile(archive_path):
+        print('archive file does not exist...')
         return False
     try:
-        gzip = archive_path.replace('/', ' ')
-        gzip = shlex.split(gzip)
-        gzip = gzip[-1]
-        xid = gzip.replace('.', ' ')
-        xid = shlex.split(xid)
-        xid = xid[0]
-
-        releases_path = "/data/web_static/releases/{}/".format(xid)
-        tmp_path = "/tmp/{}".format(gzip)
-
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(releases_path))
-        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
-        run("rm {}".format(tmp_path))
-        run("mv {}web_static/* {}".format(releases_path, releases_path))
-        run("rm -rf {}web_static".format(releases_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(releases_path))
-        print("New version deployed!")
-        return True
-    except:
+        gzip = archive_path.split('/')[1]
+        sans_gzip = gzip.split('.')[0]
+    except Exception as e:
+        print('Unable to get archive name')
         return False
+
+    uploaded = put(archive_path, '/tmp/')
+    if uploaded.failed:
+        return False
+    res = run('mkdir -p /data/web_static/releases/{}/'.format(sans_gzip))
+    if res.failed:
+        return False
+    res = run('tar -C /data/web_static/releases/{} -xzf /tmp/{}'.format(
+               sans_gzip, gzip))
+    if res.failed:
+        print('Archive opening unsuccessful')
+        return False
+    res = run('rm /tmp/{}'.format(gzip))
+    if res.failed:
+        print('Unable to delete archive...')
+        return False
+    res = run('mv /data/web_static/releases/{}/web_static/* \
+               /data/web_static/releases/{}'
+              .format(sans_gzip, sans_gzip))
+    if res.failed:
+        return False
+    res = run('rm -rf /data/web_static/releases/{}/web_static'
+              .format(sans_gzip))
+    if res.failed:
+        return False
+    res = run('rm -rf /data/web_static/current')
+    if res.failed:
+        return False
+    res = run('ln -sfn /data/web_static/releases/{} /data/web_static/current'
+              .format(sans_gzip))
+    if res.failed:
+        return False
+    print('\nNew Version Deployed!\n')
+
+    return True

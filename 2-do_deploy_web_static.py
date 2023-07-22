ce8352163ea5aea69b94cc99948f13bf54fffb0d
TriremeSolutions <themellowread@gmail.com>
2023-07-22 23:41:39 +0100

    recode 2-deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -12,28 +12,34 @@ env.hosts = ['100.26.244.129', '52.204.216.209']
 
 def do_deploy(archive_path):
     """ Search and deploy archive to server """
-    if not os.path.exists(archive_path):
-        return False
-    try:
-        gzip = archive_path.replace('/', ' ')
-        gzip = shlex.split(gzip)
-        gzip = gzip[-1]
-        xid = gzip.replace('.', ' ')
-        xid = shlex.split(xid)
-        xid = xid[0]
 
-        releases_path = "/data/web_static/releases/{}/".format(xid)
-        tmp_path = "/tmp/{}".format(gzip)
-
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(releases_path))
-        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
-        run("rm {}".format(tmp_path))
-        run("mv {}web_static/* {}".format(releases_path, releases_path))
-        run("rm -rf {}web_static".format(releases_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(releases_path))
-        print("New version deployed!")
-        return True
-    except:
+    if os.path.isfile(archive_path) is False:
         return False
+    gzip = archive_path.split("/")[-1]
+    capt = gzip.split(".")[0]
+
+    if put(archive_path, "/tmp/{}".format(gzip)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(capt)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(capt)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(gzip, capt)).failed is True:
+        return False
+    if run("rm /tmp/{}".format(gzip)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(capt, capt)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(capt)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(capt)).failed is True:
+        return False
+    return True

4b738b62d80f75daace9c320faea9f4adcc7bf50
TriremeSolutions <themellowread@gmail.com>
2023-08-01 06:57:03 +0100

    setting up routes. States list.

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """displays text 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """displays text 'HBNB'"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """displays 'C' and the <text> input value."""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """displays 'Python' and the <text> input value."""
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """displays text only if n is an integer"""
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """displays text 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """displays text 'HBNB'"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """displays 'C' and the <text> input value."""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """displays 'Python' and the <text> input value."""
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """displays text only if n is an integer"""
+    return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """displays HTML page only if n is an integer"""
+    return render_template("5-number.html", n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    """
+    displays HTML if <n> is integer
+    declares if n is even or odd
+    """
+    return render_template('6-number_odd_or_even.html', number=n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)

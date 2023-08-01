ca70ed8f08025d47cce53c5792de209b1f079a4e
TriremeSolutions <themellowread@gmail.com>
2023-08-01 11:12:51 +0100

    states by id route

---
+++ web_flask/9-states.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+from models import storage
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states():
+    """
+    displays HTML page with list of States
+    sorted alphabetically
+    """
+    states = storage.all("State")
+    return render_template("9-states.html", states=states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id):
+    """
+    displays HTML of States with exisiting id info
+    """
+    for state in storage.all("State").values():
+        if state.id == id:
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html") 
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """close SQLAlc session running"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
